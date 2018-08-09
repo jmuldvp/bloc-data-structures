8c7e8ab430e11a7c1c349e5e371c23536f63456f
jmuldvp <jmul.dvp@gmail.com>
2018-08-09 08:16:05 -0600

    Rspec issue

--- 02-stacks-and-queues/mystack/mystack.rb
+++ 02-stacks-and-queues/mystack/mystack.rb
@@ -7,11 +7,18 @@ class MyStack
   end
 
   def push(item)
+    self.top = item
+    @stack[0] = self.top
   end
 
   def pop
+    temp = self.top
+    @stack.delete_at(@stack.length - 1)
+    self.top = @stack[@stack.length - 1]
+    return temp
   end
 
   def empty?
+    !@stack[0] ? true : false
   end
 end
