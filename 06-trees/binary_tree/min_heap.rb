f290406d2a04d08cc4ce2a75c7ffca5ee63d1039
jmuldvp <jmul.dvp@gmail.com>
2019-01-16 22:38:17 -0700

    Finished min heap. Need to fix spec errors.

--- 06-trees/binary_tree/min_heap.rb
+++ 06-trees/binary_tree/min_heap.rb
@@ -9,25 +9,58 @@ class MinHeap
 
   def insert(root, node)
     if root.rating > node.rating
-      @root, temp, node = node, root, temp
+      temp, @root, node = root, node, temp
+      insert(@root, node)
     else
-
+      if root.left.nil?
+        root.left = node
+      elsif root.right.nil? && !root.left.nil?
+        root.right = node
+      elsif !root.left.nil? && !root.right.nil? && !root.left.left.nil? && !root.left.right.nil?
+        insert(root.right, node)
+      elsif !root.left.nil? && !root.right.nil?
+        insert(root.left, node)
+      end
     end
   end
 
   def find(root, data)
+    return nil if data.nil? || root.nil?
     if root.title == data
       return root
+    else
+      node = find(root.left, data)
+      if node.nil?
+        node = find(root.right, data)
+      end
     end
-
+    return node
   end
 
   def delete(root, data)
-
+    if data.nil? || root.nil?
+      return nil
+    else
+      target_node = find(root, data)
+      target_node.nil? ? nil : (target_node.title = nil && target_node.rating = nil)
+    end
   end
 
   def printf(children=nil)
-
+    que, res = [@root], []
+    while que.length > 0
+      new_root = que.shift
+      return que.push(new_root.left) if !new_root.left.nil?
+      # if new_root.left != nil
+      #   que.push(new_root.left)
+      # end
+      return que.push(new_root.right) if !new_root.right.nil?
+      # if new_root.right != nil
+      #   que.push(new_root.right)
+      # end
+      res.push("#{new_root.title}: #{new_root.rating}")
+    end
+    res.each {|x| puts x}
   end
 
 end
--- 06-trees/binary_tree/min_heap_spec.rb
+++ 06-trees/binary_tree/min_heap_spec.rb
@@ -3,5 +3,79 @@ include RSpec
 require_relative 'min_heap'
 
 RSpec.describe MinHeap, type: Class do
-  
+  let (:root) { Node.new("Band of Brothers", 87) }
+
+  let (:tree) { MinHeap.new(root) }
+  let (:ryan) { Node.new("Saving Private Ryan", 72) }
+  let (:braveheart) { Node.new("Braveheart", 78) }
+  let (:jedi) { Node.new("Star Wars: Return of the Jedi", 80) }
+  let (:heaven) { Node.new("My Blue Heaven", 85) }
+  let (:inception) { Node.new("Inception", 86) }
+  let (:solo) { Node.new("Solo", 90) }
+  let (:nobody) { Node.new("My Name Is Nobody", 91) }
+  let (:ldr) { Node.new("Live, Die, Repeat", 92) }
+  let (:beasts) { Node.new("Fantastic Beasts", 93) }
+  let (:bourneid) { Node.new("Bourne Identity", 94) }
+  let (:oceans11) { Node.new("Oceans 11", 98) }
+
+  describe "#insert(data)" do
+    it "properly inserts a new node and it swaps with the root" do
+      tree.insert(root, ryan)
+      expect(tree.root.title).to eq "Saving Private Ryan"
+    end
+
+    it "properly inserts a new node and it swaps with the root" do
+      tree.insert(root, ryan)
+      expect(tree.root.left.title).to eq "Band of Brothers"
+    end
+
+    it "properly inserts a new node as a left-left child" do
+      tree.insert(tree.root, braveheart)
+      tree.insert(tree.root, ryan)
+      expect(tree.root.left.left.title).to eq "Band of Brothers"
+    end
+
+    it "properly inserts a new node as a left-right child" do
+      tree.insert(tree.root, heaven)
+      tree.insert(tree.root, inception)
+      expect(tree.root.right.title).to eq "Inception"
+    end
+
+    it "properly inserts a new node as a right child" do
+      tree.insert(tree.root, ryan)
+      tree.insert(tree.root, solo)
+      expect(tree.root.right.title).to eq "Solo"
+    end
+
+    it "properly inserts a new node as a right-left child" do
+      tree.insert(tree.root, ryan)
+      tree.insert(tree.root, solo)
+      tree.insert(tree.root, beasts)
+      tree.insert(tree.root, ldr)
+      tree.insert(tree.root, oceans11)
+      expect(tree.root.right.left.title).to eq "Oceans 11"
+    end
+
+    it "properly inserts a new node as a right-right child" do
+      tree.insert(tree.root, ryan)
+      tree.insert(tree.root, solo)
+      tree.insert(tree.root, beasts)
+      tree.insert(tree.root, ldr)
+      tree.insert(tree.root, oceans11)
+      tree.insert(tree.root, bourneid)
+      expect(tree.root.right.right.title).to eq "Bourne Identity"
+    end
+  end
+
+  describe "#print" do
+    it "should print" do
+      tree.insert(tree.root, ryan)
+      tree.insert(tree.root, solo)
+      tree.insert(tree.root, beasts)
+      tree.insert(tree.root, ldr)
+      tree.insert(tree.root, oceans11)
+      tree.insert(tree.root, bourneid)
+      tree.printf()
+   end
+  end
 end
